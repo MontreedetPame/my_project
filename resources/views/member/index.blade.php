3c388e16fdb315085c300d2d12fc0cffd6968255
MontreedetPame <zoozoro@hotmail.com>
2022-07-24 21:36:37 +0700

    การเพิ่ม sub folder ของ view และการ route

--- resources/views/member/index.blade.php
+++ resources/views/member/index.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>เกี่ยวกับเรา</title>
+    <title>หน้าสมาชิก</title>
 </head>
 <body>
-    หน้าเกี่ยวกับเรา
+    หน้าแสดงส่วนของสมาชิก
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/member', function () {
+    return view('member.index');
+});
+
+Route::get('/admin', function () {
+    return view('admin.index');
 });
